8abac6b71c30c77e96c89c8eba34a8f27467d0a2
Adel <57968261+dedely@users.noreply.github.com>
2020-11-27 18:42:16 +0100

    update ddl, add sample dataset

--- data/ddl.sql
+++ data/ddl.sql
@@ -2,32 +2,33 @@
 -- File:        ddl.sql
 -- Description: ddl for our museen project
 -- Authors: Aëlien MOUBECHE, Adel ABBAS
--- Created:     2020-11-03
+-- Created:     2020-11-27
 --========================================================================
 
 CREATE TABLE pricing
 (
-    pricing_id          CHAR(3),
+    pricing_id          CHAR(4),
     pricing_price       FLOAT NOT NULL,
-    pricing_name        VARCHAR(10),
+    pricing_name        VARCHAR(20),
     pricing_description VARCHAR(50),
     CONSTRAINT pricing_pk PRIMARY KEY (pricing_id)
 );
 
 CREATE TABLE subscription
 (
-    subscription_id       CHAR(3),
-    subscription_duration INT     NOT NULL,
-    subscription_renewed  BOOLEAN NOT NULL,
+    subscription_id       CHAR(4)
+        CONSTRAINT subscription_id_format CHECK (subscription_id LIKE 'S%'),
+    subscription_duration INT NOT NULL,
     CONSTRAINT subscription_pk PRIMARY KEY (subscription_id),
     CONSTRAINT subscription_fk FOREIGN KEY (subscription_id) REFERENCES pricing (pricing_id)
 );
 
 CREATE TABLE guided_tour
 (
-    gt_id         CHAR(3),
+    gt_id         CHAR(4)
+        CONSTRAINT gt_id_format CHECK (gt_id LIKE 'G%'),
     gt_guide_name VARCHAR(15) NOT NULL,
-    gt_date       CHAR(2)        NOT NULL,
+    gt_day        CHAR(2)     NOT NULL,
     gt_hour       TIME        NOT NULL,
     gt_place      VARCHAR(15) NOT NULL,
     CONSTRAINT gt_pk PRIMARY KEY (gt_id),
@@ -36,37 +37,38 @@ CREATE TABLE guided_tour
 
 CREATE TABLE visitor
 (
-    visitor_id         CHAR(10),
-    visitor_first_name VARCHAR(20) NOT NULL,
-    visitor_last_name  VARCHAR(20) NOT NULL,
-    visitor_birthdate  DATE        NOT NULL,
-    visitor_pricing    CHAR(3),
-    CONSTRAINT visitor_pk PRIMARY KEY (visitor_id),
-    CONSTRAINT visitor_fk FOREIGN KEY (visitor_pricing) REFERENCES pricing (pricing_id)
+    visitor_id            VARCHAR(20),
+    visitor_first_name    VARCHAR(20)         NOT NULL,
+    visitor_last_name     VARCHAR(20)         NOT NULL,
+    visitor_birthdate     DATE                NOT NULL,
+    visitor_password_hash VARCHAR(255)        NOT NULL,
+    visitor_authkey_hash  VARCHAR(255) UNIQUE NOT NULL,
+    CONSTRAINT visitor_pk PRIMARY KEY (visitor_id)
 );
 
 CREATE TABLE is_paying
 (
-    ip_visitor_id       CHAR(10),
-    ip_pricing_id       CHAR(3),
-    ip_reservation_date DATE NOT NULL,
-    ip_payment_date     DATE NOT NULL,
-    CONSTRAINT ip_pk PRIMARY KEY (ip_visitor_id, ip_pricing_id),
+    ip_visitor_id       VARCHAR(20),
+    ip_pricing_id       CHAR(4),
+    ip_reservation_date DATE,
+    ip_payment_date     DATE    NOT NULL,
+    ip_auto_renewed     BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT ip_pk PRIMARY KEY (ip_visitor_id, ip_pricing_id, ip_reservation_date),
     CONSTRAINT ip_fk_visitor FOREIGN KEY (ip_visitor_id) REFERENCES visitor (visitor_id),
     CONSTRAINT ip_fk_pricing FOREIGN KEY (ip_pricing_id) REFERENCES pricing (pricing_id)
 );
 
 CREATE TABLE location
 (
-    loc_id        CHAR(8),
+    loc_id        SERIAL,
     loc_room_name VARCHAR(30) NOT NULL,
     CONSTRAINT location_pk PRIMARY KEY (loc_id)
 );
 
 CREATE TABLE location_history
 (
-    location_visitor_id CHAR(10),
-    location_id         CHAR(8),
+    location_visitor_id VARCHAR(20),
+    location_id         INTEGER,
     location_time_in    TIMESTAMP,
     location_time_out   TIMESTAMP,
     CONSTRAINT location_history_pk PRIMARY KEY (location_visitor_id, location_id, location_time_in),
@@ -76,18 +78,18 @@ CREATE TABLE location_history
 
 CREATE TABLE artist
 (
-    artist_id                CHAR(8),
-    artist_name              VARCHAR(20) DEFAULT 'Artiste Inconnu' NOT NULL,
+    artist_id                SERIAL,
+    artist_name              VARCHAR(20) NOT NULL,
     artist_birth             DATE,
     artist_death             DATE,
-    artist_artistic_movement CHAR(3),
+    artist_artistic_movement INTEGER,
     artist_bio               VARCHAR(150),
     CONSTRAINT artist_pk PRIMARY KEY (artist_id)
 );
 
 CREATE TABLE artistic_movement
 (
-    artistic_movement_id          CHAR(3),
+    artistic_movement_id          SERIAL,
     artistic_movement_name        VARCHAR(30) NOT NULL,
     artistic_movement_lead_artist VARCHAR(30) NOT NULL,
     CONSTRAINT artistic_movement_pk PRIMARY KEY (artistic_movement_id)
@@ -97,13 +99,14 @@ CREATE TYPE art_type AS ENUM ('Sculpture', 'Tableau');
 
 CREATE TABLE artwork
 (
-    artwork_id          CHAR(11),
+    artwork_id          SERIAL,
+    artwork_title       VARCHAR(30) NOT NULL,
     artwork_type        art_type    NOT NULL,
-    artwork_artist      CHAR(8)     NOT NULL,
-    artwork_date        VARCHAR(10) NOT NULL,
-    artwork_movement_id CHAR(3)     NOT NULL,
-    artwork_popularity  INT,
-    artwork_location    CHAR(8)     NOT NULL,
+    artwork_artist      INTEGER     NOT NULL,
+    artwork_date        VARCHAR(10) NOT NULL, --Not a DATE type
+    artwork_movement_id INTEGER     NOT NULL,
+    artwork_popularity  INTEGER,
+    artwork_location    INTEGER     NOT NULL,
     CONSTRAINT artwork_pk PRIMARY KEY (artwork_id),
     CONSTRAINT artwork_fk_artist FOREIGN KEY (artwork_artist) REFERENCES artist (artist_id),
     CONSTRAINT artwork_fk_movement FOREIGN KEY (artwork_movement_id) REFERENCES artistic_movement (artistic_movement_id)
@@ -111,8 +114,8 @@ CREATE TABLE artwork
 
 CREATE TABLE artist_preference
 (
-    ap_visitor_id CHAR(10),
-    ap_artist_id  CHAR(8),
+    ap_visitor_id VARCHAR(20),
+    ap_artist_id  INTEGER,
     CONSTRAINT artist_preference_pk PRIMARY KEY (ap_visitor_id, ap_artist_id),
     CONSTRAINT artist_preference_fk_visitor FOREIGN KEY (ap_visitor_id) REFERENCES visitor (visitor_id),
     CONSTRAINT artist_preference_fk_artist FOREIGN KEY (ap_artist_id) REFERENCES artist (artist_id)
@@ -120,10 +123,36 @@ CREATE TABLE artist_preference
 
 CREATE TABLE artistic_movement_preference
 (
-    amp_visitor_id           CHAR(10),
-    amp_artistic_movement_id CHAR(4),
+    amp_visitor_id           VARCHAR(20),
+    amp_artistic_movement_id INTEGER,
     amp_score                INT NOT NULL,
     CONSTRAINT artist_movement_preference_pk PRIMARY KEY (amp_visitor_id, amp_artistic_movement_id),
     CONSTRAINT artist_movement_preference_fk_visitor FOREIGN KEY (amp_visitor_id) REFERENCES visitor (visitor_id),
     CONSTRAINT artist_movement_preference_fk_artist FOREIGN KEY (amp_artistic_movement_id) REFERENCES artistic_movement (artistic_movement_id)
 );
+
+INSERT INTO pricing (pricing_id, pricing_price, pricing_name, pricing_description)
+VALUES ('POO1', 10, 'Entrée simple', 'Entrée simple'),
+       ('POO2', 5, 'Entrée tarif réduit', 'Tarif réduit (-50%)'),
+       ('S001', 100, 'Abonnement annuel', 'Accès illimité au musée pendant 1 an'),
+       ('G001', 15, 'Visite guidée', 'Visite guidée');
+
+INSERT INTO subscription (subscription_id, subscription_duration)
+VALUES ('S001', 365);
+
+INSERT INTO guided_tour(gt_id, gt_guide_name, gt_day, gt_hour, gt_place)
+VALUES ('G001', 'Gilles', 'Lu', '09:00', 'Hall');
+
+INSERT INTO visitor(visitor_id, visitor_first_name, visitor_last_name, visitor_birthdate, visitor_password_hash,
+                    visitor_authkey_hash)
+VALUES ('jtest', 'Jean', 'Test', '2020-01-01', 'museen', 'museen-artwork-secrets-exposed');
+
+INSERT INTO artistic_movement(artistic_movement_name, artistic_movement_lead_artist) VALUES
+('Surréalisme', 'André Breton');
+
+INSERT INTO artist(artist_name, artist_birth, artist_death, artist_artistic_movement, artist_bio)
+VALUES ('René Magritte', '1898-11-21', '1967-08-15', 1,
+        'René Magritte, né le 21 novembre 1898 à Lessines dans le Hainaut (Belgique) et mort à Bruxelles le 15 août 1967, est un peintre surréaliste belge.');
+INSERT INTO artwork(artwork_title, artwork_type, artwork_artist, artwork_date, artwork_movement_id,
+                    artwork_location)
+VALUES ('La Trahison des images', 'Tableau', 1, '1928–1929', 1, 1);
